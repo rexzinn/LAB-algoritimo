85fb995206148189374b52b5b56f74f3475eb49d
rexzinn <99694987+rexzinn@users.noreply.github.com>
2023-08-31 16:14:10 -0300

    i

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
+nome = int(input(" Digite seu nome:"))
 
-idade = 18
-nome = "Gabriel Rosa de Camargo"
-cpf = "000.000.000-00"
-peso = 70
-dataDeNascimento = "2004-08-21"
-cidade = "Cachoeira do Sul"
-estado = "RS"
-velocidade = 60
-preco = 108.50
 
-print("Idade:", idade)
-print("Nome:", nome)
-print("CPF:", cpf)
-print("Peso:", peso)
-print("Data de Nascimento:", dataDeNascimento)
-print("Cidade:", cidade)
-print("Estado:", estado)
-print("Velocidade:", velocidade)
-print("Preço:", preco)
+print (nome)
 
 
 
